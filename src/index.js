53dabbc7dafe8adcc6b3644c8e14534284221d8c
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-04-02 21:06:51 +0200

    create the board with two nested for loops

--- src/index.js
+++ src/index.js
@@ -20,26 +20,23 @@ class Board extends React.Component {
         )
     }
 
+    createBoard = () => {
+        let board = []
+
+        for (let r = 0; r < 3; r++) {
+            let children = []
+            for (let c = 0; c < 3; c++) {
+                children.push(this.renderSquare(r * 3 + c))
+            }
+
+            board.push(<div className="board-row">{children}</div>)
+        }
+
+        return board
+    }
+
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        )
+        return <div>{this.createBoard()}</div>
     }
 }
 
